ac6c5ac986ab41df16d8c1b8a3c4beb74312d823
Jason Morgan <jason.morgan@acceleration.biz>
2020-08-06 20:36:11 -0400

    Adding pip build

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="adobe-analytics-api_20",
+    version="0.0.1",
+    author="Jason Morgan",
+    author_email="jason@framingeinstein.com",
+    description="Adobe Analytics API 2.0 Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/framingeinstein/adobe-analytics-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
